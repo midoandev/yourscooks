bdcf4924744157d00dd0708ca8f8aa8716d2b8d0
mido <mido@digitalsekuriti.id>
2024-12-17 10:44:59 +0700

    fix: update git ignore and test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,11 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:yourscooks/core/app_component.dart';
-import 'package:yourscooks/core/env.dart';
 
-import 'package:yourscooks/main.dart';
-import 'package:yourscooks/utility/shared/constants/app_theme.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
